6df6061dc6e9348503883dfdf2dbca1292774377
SML <smlbiobot@gmail.com>
2024-10-08 18:00:08 +0800

    Update to flux pro 1.1

--- nodes/replicate_node.py
+++ nodes/replicate_node.py
@@ -27,14 +27,17 @@ class SMLFluxProReplicateNode(SMLFluxBaseNode):
             "required": {
                 "prompt": ("STRING", {"default": "", "multiline": True}),
                 "aspect_ratio": (
-                    ["1:1", "16:9", "2:3", "3:2", "4:5", "5:4", "9:16"],
+                    ["1:1", "16:9", "2:3", "3:2", "4:5", "5:4", "9:16", "3:4", "4:3", "custom"],
                     {"default": "1:1"}
                 ),
+                "width": ("INT", {"default": 1024, "min": 256, "max": 1440}),
+                "height": ("INT", {"default": 1024, "min": 256, "max": 1440}),
                 "steps": ("INT", {"default": 30, "min": 1, "max": 100}),
                 "output_quality": ("INT", {"default": 100, "min": 1, "max": 100}),
                 "guidance": ("FLOAT", {"default": 3, "min": 2, "max": 5}),
                 "interval": ("INT", {"default": 2, "min": 1, "max": 4}),
                 "safety_tolerance": ("INT", {"default": 5, "min": 1, "max": 5}),
+                "prompt_upsampling": ("BOOL", {"default": False}),
             },
             "optional": {
                 "seed": ("INT", {"default": -1}),
@@ -48,23 +51,29 @@ class SMLFluxProReplicateNode(SMLFluxBaseNode):
     def generate_image(self,
                        prompt,
                        aspect_ratio,
+                       width,
+                       height,
                        steps,
                        output_quality,
                        guidance,
                        interval,
                        safety_tolerance,
+                       prompt_upsampling,
                        seed=-1
                        ):
         input = dict(
             prompt=prompt,
             aspect_ratio=aspect_ratio,
             output_quality=output_quality,
+            width=width,
+            height=height,
             output_format='png',
             steps=steps,
             safety_tolerance=safety_tolerance,
             guidance=guidance,
             interval=interval,
             disable_safety_checker=True,
+            prompt_upsampling=prompt_upsampling,
         )
         if seed != -1:
             input["seed"] = seed
@@ -72,7 +81,7 @@ class SMLFluxProReplicateNode(SMLFluxBaseNode):
         try:
 
             result = replicate.run(
-                "black-forest-labs/flux-pro",
+                "black-forest-labs/flux-1.1-pro",
                 input=input,
             )
             return self.process_result(result)
@@ -88,5 +97,5 @@ NODE_CLASS_MAPPINGS = {
 
 # Node display name mappings
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "SML_FluxPro_Replicate_Standalone": "😃 SML Flux Pro (Replicate)",
+    "SML_FluxPro_Replicate_Standalone": "😃 SML Flux Pro 1.1 (Replicate)",
 }
