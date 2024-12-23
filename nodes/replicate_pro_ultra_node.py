1e34aa3f70b4371aea496b90fab57c80b8ad9b1f
SML <smlbiobot@gmail.com>
2024-12-23 08:54:18 +0800

    add replicate pro ultra node

--- nodes/replicate_pro_ultra_node.py
+++ nodes/replicate_pro_ultra_node.py
@@ -6,7 +6,7 @@ import replicate
 from .base_node import SMLFluxBaseNode
 
 
-class SMLFluxProReplicateNode(SMLFluxBaseNode):
+class SMLFluxProUltraReplicateNode(SMLFluxBaseNode):
     api_token: str = None
 
     @classmethod
@@ -15,17 +15,32 @@ class SMLFluxProReplicateNode(SMLFluxBaseNode):
             "required": {
                 "prompt": ("STRING", {"default": "", "multiline": True}),
                 "aspect_ratio": (
-                    ["1:1", "16:9", "2:3", "3:2", "4:5", "5:4", "9:16", "3:4", "4:3", "custom"],
-                    {"default": "1:1"}
+                    [
+                        "21:9",
+                        "16:9",
+                        "3:2",
+                        "4:3",
+                        "5:4",
+                        "1:1",
+                        "4:5",
+                        "3:4",
+                        "2:3",
+                        "9:16",
+                        "9:21",
+                        "custom"
+                    ],
+                    {
+                        "default": "1:1"
+                    }
                 ),
                 "width": ("INT", {"default": 1024, "min": 256, "max": 1440}),
                 "height": ("INT", {"default": 1024, "min": 256, "max": 1440}),
                 "steps": ("INT", {"default": 30, "min": 1, "max": 100}),
                 "output_quality": ("INT", {"default": 100, "min": 1, "max": 100}),
-                "guidance": ("FLOAT", {"default": 3, "min": 2, "max": 5}),
+                "image_prompt_strength": ("FLOAT", {"default": 0.1, "min": 0.0, "max": 1.0}),
                 "interval": ("INT", {"default": 2, "min": 1, "max": 4}),
                 "safety_tolerance": ("INT", {"default": 6, "min": 1, "max": 6}),
-                "prompt_upsampling": ("BOOLEAN", {"default": False}),
+                "raw": ("BOOLEAN", {"default": False}),
             },
             "optional": {
                 "seed": ("INT", {"default": -1}),
@@ -88,7 +103,7 @@ class SMLFluxProReplicateNode(SMLFluxBaseNode):
         try:
 
             result = replicate.run(
-                "black-forest-labs/flux-1.1-pro",
+                "black-forest-labs/flux-1.1-pro-ultra",
                 input=input,
             )
 
@@ -101,10 +116,10 @@ class SMLFluxProReplicateNode(SMLFluxBaseNode):
 
 # Node class mappings
 NODE_CLASS_MAPPINGS = {
-    "SML_FluxPro_Replicate_Standalone": SMLFluxProReplicateNode,
+    "SML_FluxProUltra_Replicate_Standalone": SMLFluxProUltraReplicateNode,
 }
 
 # Node display name mappings
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "SML_FluxPro_Replicate_Standalone": "😃 SML Flux Pro 1.1 (Replicate)",
+    "SML_FluxProUltra_Replicate_Standalone": "😃 SML Flux Pro Ultra 1.1 (Replicate)",
 }
